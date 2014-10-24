06ecb97b195520aa201a942212c268fd2a2aaee9
Juan <jcgf251@hotmail.com>
2014-10-24 00:19:18 -0500

    Inicio proyecto

---
+++ MonkeySoft/src/monkeysoft/MonkeySoft.java
@@ -0,0 +1,16 @@
+package monkeysoft;
+
+/**
+ *
+ * @author Juan Camilo
+ */
+public class MonkeySoft {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
