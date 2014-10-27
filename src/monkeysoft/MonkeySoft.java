2a7d9c1efd280957e0be4036d088404356651ff2
Juan <jcgf251@hotmail.com>
2014-10-27 14:03:52 -0500

    Mejora try catch

--- src/monkeysoft/MonkeySoft.java
+++ src/monkeysoft/MonkeySoft.java
@@ -2,6 +2,7 @@ package monkeysoft;
 
 import javax.swing.JOptionPane;
 import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 import principal.Log;
 
 /**
@@ -16,7 +17,7 @@ public class MonkeySoft {
     public static void main(String[] args) {
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (Exception e) {
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
             JOptionPane.showMessageDialog(null, e.getMessage(), MonkeySoft.class.getName(), JOptionPane.ERROR_MESSAGE);
         }
         Log login = new Log();
